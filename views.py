125b6095282c83e814617cb4a9c2b2110ade275a
gregroberts <greg.roberts1991@gmail.com>
2017-04-16 22:19:01 +0100

    Basic Structure

---
+++ views.py
@@ -0,0 +1,86 @@
+from flask import Flask, request, Response, abort, render_template
+from flask.ext.classy import FlaskView, route
+from json import dumps
+import neo
+
+
+class ArticlesView(FlaskView):
+	def index(self):
+		arts = neo.get_nodes('Article', 10)
+		return render_template(
+			'articles.html',
+			articles=arts
+			)
+
+	def get(self, id):
+		art = neo.get_node(id)
+		sources = neo.get_article_sources(art['url'])
+		topics = neo.get_article_topics(art['url'])
+		return render_template(
+			'article.html',
+			sources=sources,
+			nSources=len(sources),
+			topics=topics,
+			**art
+			)
+
+class TopicsView(FlaskView):
+	def index(self):
+		topics = neo.get_nodes('Topic',order='name',limit=10)
+		return render_template(
+			'topics.html',
+			topics=topics
+			)
+
+	def get(self, id):
+		topic = neo.get_node(id)
+		articles = neo.get_topic_articles(topic['name'])
+		return render_template(
+			'topic.html',
+			articles=articles,
+			**topic
+			)
+
+class DomainsView(FlaskView):
+	def index(self):
+		domains = neo.get_nodes('Domain',order='articles',  limit=100)
+		print domains
+		return render_template(
+			'domains.html',
+			domains=domains
+			)
+
+	def get(self, id):
+		domain = dict(neo.get_node(id).items())
+		articles = neo.get_domain_articles(domain['domain'])
+		domain['nArticles'] = len(articles)
+		domain['articles'] = articles
+		return render_template(
+			'domain.html',
+			**domain
+			)
+
+
+app = Flask(__name__)
+ArticlesView.register(app)
+TopicsView.register(app)
+DomainsView.register(app)
+
+@app.errorhandler(500)
+def internal_error(e):
+	print e
+	return Response(
+		response =dumps({'error':str(e)}),
+		status = 500,
+		mimetype = 'application/json'
+	)
+
+if __name__ == '__main__':
+	def internal_error(e):
+		print e
+		return Response(
+			response =dumps({'error':str(e)}),
+			status = 500,
+			mimetype = 'application/json'
+		)
+	app.run(host='0.0.0.0', debug=True)
